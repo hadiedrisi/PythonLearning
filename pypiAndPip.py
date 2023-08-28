4cd4ff2c4ca4ce55bc303c8ffde852f90247ed9f
hadi-hedco <edrisi.hadi@hedcoint.com>
2023-08-28 16:10:52 +0330

    read excell file

--- pypiAndPip.py
+++ pypiAndPip.py
@@ -1,1 +1,16 @@
 #pypi.org
+import openpyxl as xl
+
+wb = xl.load_workbook("test.xlsx")
+# wb['Sheet1'].title = "test"
+# wb.save("./public/test.xlsx")
+sheet = wb['Sheet1']
+cell = sheet['A1']
+cell2 = sheet.cell(row=1, column=2)
+print(cell.value)
+print(cell2.value)
+
+for row in range(1, sheet.max_row+1):
+    for col in range(1, sheet.max_column+1):
+        cell = sheet.cell(row, col)
+        print(cell.value)
